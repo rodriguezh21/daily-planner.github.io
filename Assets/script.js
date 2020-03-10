71200690cd9520ca38983828bca26079b3428dec
rodriguezh21 <rodriguezh21@gmail.com>
2020-03-09 23:52:35 -0500

    progress

---
+++ Assets/script.js
@@ -0,0 +1,66 @@
+$(document).ready(); {
+    
+    // Displays the Current Day
+    $("#currentDay").text('Today is ' + moment().format('dddd'));
+
+    // Gets the input value and stores it
+        $('.saveBtn').on('click', function(e){
+            
+            var input = $(this).siblings(".event").val()
+            
+            var time = $(this).siblings(".event").attr("id")
+            
+
+            localStorage.setItem(time, input)
+           
+        })
+
+        // An attempt to change the color of the booking once clicked
+        $('saveBtn').on('click', function (){
+            $('.event').backgroundColor = red;
+        })
+        // A loop for the times 9-17
+            var displayTask = function(){
+
+            for (i=9; i<18; i++)
+
+      $(".slot #"+i+"").val(localStorage.getItem(i))
+
+
+        }
+        displayTask();
+
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
